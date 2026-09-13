8be0a9bbc7bec32a893a2cb7f5fc7b499f795712
Ferguindc <167235396+Ferguindc@users.noreply.github.com>
2026-09-13 18:33:54 -0300

    fix redirecturi azure msal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
     authority:
       'https://login.microsoftonline.com/3070fad0-b050-48d9-b664-a1cb8ddc6e0a',
 
-    redirectUri: 'http://localhost:4200/',
+    // CORRECCIÓN: Sin '/' al final para coincidir exactamente con Azure Portal
+    redirectUri: 'http://localhost:4200',
 
     api: {
       clientId: '51023582-39a3-4ed6-809b-bf819dea59a1',
@@ -16,7 +17,6 @@ export const environment = {
       scope:
         'api://20d1c6a6-0709-469d-9cf8-a8a09af6de0b/access_as_user2',
 
-      // URL en minúsculas (desarrollo):
       url: 'https://w0fjqef3m3.execute-api.us-east-1.amazonaws.com/desarrollo',
     },
   },
